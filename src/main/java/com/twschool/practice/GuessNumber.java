153d9cdaeff3c84ec6b42f1c988bc643d9cb8645
zhaohuanhuan <1297972106@qq.com>
2020-06-16 22:18:51 +0800

    通过第一个测试案例———数字和位置全都对

---
+++ src/main/java/com/twschool/practice/GuessNumber.java
@@ -0,0 +1,10 @@
+package com.twschool.practice;
+
+public class GuessNumber {
+    public String getGuessResult(String answerStr, String inputStr) {
+        if(answerStr.equals(inputStr)){
+            return "4A0B";
+        }
+        return "";
+    }
+}
--- src/test/java/com/twschool/practice/GuessNumberTest.java
+++ src/test/java/com/twschool/practice/GuessNumberTest.java
@@ -2,10 +2,17 @@ package com.twschool.practice;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class GuessNumberTest {
     @Test
     public void should_return_4A0B_when_input_num_and_position_all_are_right(){
-//        GuessNumber guessNumber = new GuessNumber();
+        //given
+        GuessNumber guessNumber = new GuessNumber();
+        //when
+        String resultString = guessNumber.getGuessResult("1,2,3,4", "1,2,3,4");
+        //then
+        assertEquals("4A0B", resultString);
     }
     @Test
     public void should_return_2A2B_when_input_nums_are_all_right_but_positions_are_not_all_right(){
