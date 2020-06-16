6366c70843395b1288143a2c1cadb5b6c43bcf84
zhaohuanhuan <1297972106@qq.com>
2020-06-16 23:19:29 +0800

    通过第三个案例———部分对，位置部分对

--- src/main/java/com/twschool/practice/GuessNumber.java
+++ src/main/java/com/twschool/practice/GuessNumber.java
@@ -6,8 +6,8 @@ public class GuessNumber {
             return "4A0B";
         }
 
-        String[] answerList= answerStr.split(",");
-        String[] inputList= inputStr.split(",");
+        String[] answerList= answerStr.split("");
+        String[] inputList= inputStr.split("");
         int countsOfNumsRight = 0;
         int countsOfNumsPositionsRight = 0;
         for(int i = 0; i < inputList.length; i++) {
--- src/test/java/com/twschool/practice/GuessNumberTest.java
+++ src/test/java/com/twschool/practice/GuessNumberTest.java
@@ -10,7 +10,7 @@ public class GuessNumberTest {
         //given
         GuessNumber guessNumber = new GuessNumber();
         //when
-        String resultString = guessNumber.getGuessResult("1,2,3,4", "1,2,3,4");
+        String resultString = guessNumber.getGuessResult("1234", "1234");
         //then
         assertEquals("4A0B", resultString);
     }
@@ -19,15 +19,27 @@ public class GuessNumberTest {
         //given
         GuessNumber guessNumber = new GuessNumber();
         //when
-        String resultString = guessNumber.getGuessResult("1,2,3,4", "1,3,2,4");
+        String resultString = guessNumber.getGuessResult("1234", "1324");
         //then
         assertEquals("2A2B", resultString);
     }
     @Test
     public void should_return_1A2B_when_input_nums_and_positions_are_not_all_right(){
+        //given
+        GuessNumber guessNumber = new GuessNumber();
+        //when
+        String resultString = guessNumber.getGuessResult("1234", "1345");
+        //then
+        assertEquals("1A2B", resultString);
     }
     @Test
     public void should_return_0A4B_when_input_nums_are_all_right_but_positions_are_all_wrong(){
+//        //given
+//        GuessNumber guessNumber = new GuessNumber();
+//        //when
+//        String resultString = guessNumber.getGuessResult("1234", "4321");
+//        //then
+//        assertEquals("0A4B", resultString);
     }
     @Test
     public void should_return_0A3B_when_input_nums_are_not_all_right_but_positions_all_are_wrong(){
