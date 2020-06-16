554ec1327ba43c5f0ca6af79d1f8fd515f99fd63
zhaohuanhuan <1297972106@qq.com>
2020-06-16 22:06:24 +0800

    初始化测试案例

---
+++ src/test/java/com/twschool/practice/GuessNumberTest.java
@@ -0,0 +1,25 @@
+package com.twschool.practice;
+
+import org.junit.Test;
+
+public class GuessNumberTest {
+    @Test
+    public void should_return_4A0B_when_input_num_and_position_all_are_right(){
+//        GuessNumber guessNumber = new GuessNumber();
+    }
+    @Test
+    public void should_return_2A2B_when_input_nums_are_all_right_but_positions_are_not_all_right(){
+    }
+    @Test
+    public void should_return_1A2B_when_input_nums_and_positions_are_not_all_right(){
+    }
+    @Test
+    public void should_return_0A4B_when_input_nums_are_all_right_but_positions_are_all_wrong(){
+    }
+    @Test
+    public void should_return_0A3B_when_input_nums_are_not_all_right_but_positions_all_are_wrong(){
+    }
+    @Test
+    public void should_return_0A0B_when_input_nums_are_all_wrong_and_positions_all_are_wrong(){
+    }
+}
